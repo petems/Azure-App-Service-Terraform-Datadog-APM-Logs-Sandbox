bcdac5c13f36ba04ce696eaabb6c7b1186504f2a
Peter Souter <petems@users.noreply.github.com>
2025-07-11 19:58:14 +0100

    Rename to new namespace

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace dotnetcoresample.Pages
+namespace AzureAppServiceSample.Pages
 {
     public class IndexModel : PageModel
     {
