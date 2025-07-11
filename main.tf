53b53b20edba1a28ebe39fcf768cbc6c668ef2ec
Peter Souter <petems@users.noreply.github.com>
2025-07-11 15:06:59 +0100

    Our Datadog Azure Sandbox is at its RG limit!
    ```
    ╷
    │ Error: creating Resource Group "myResourceGroup-44404": resources.GroupsClient#CreateOrUpdate: Failure sending request: StatusCode=409 -- Original Error: autorest/azure: Service returned an error. Status=<nil> Code="ResourceGroupQuotaExceeded" Message="Creating the resource group 'myResourceGroup-44404' would exceed the quota of '980'. The current resource group count is '980', please delete some groups before creating a new one."
    ```

--- main.tf
+++ main.tf
@@ -19,17 +19,25 @@ resource "random_integer" "ri" {
   max = 99999
 }
 
-# Create the resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup-${random_integer.ri.result}"
-  location = "eastus"
+# Create the resource group (if in your own Azure account)
+# This is not needed if you are using the Datadog sandbox repo
+# However it switches all the logic to use data instead of a created resource
+# So for now, we'll lookup the existing resource group
+# resource "azurerm_resource_group" "rg" {
+#   name     = "myResourceGroup-${random_integer.ri.result}"
+#   location = "eastus"
+# }
+
+# Use the existing default eastus resource group (using the Datadog sandbox repo)
+data "azurerm_resource_group" "rg" {
+  name = "DefaultResourceGroup-EUS"
 }
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "webapp-asp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "B1"
 }
@@ -37,8 +45,8 @@ resource "azurerm_service_plan" "appserviceplan" {
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
   name                  = "webapp-${random_integer.ri.result}"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
   depends_on            = [azurerm_service_plan.appserviceplan]
   https_only            = true
